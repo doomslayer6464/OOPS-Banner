775c31b09c380957b8991eed28cd177718a952da
doomslayer6464 <adithyan46a@gmail.com>
2026-03-03 13:23:35 +0530

    Add files via upload

---
+++ UC1.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp - UC1 Implementation
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Adithyan
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
