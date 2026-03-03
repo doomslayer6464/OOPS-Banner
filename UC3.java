276de4acdb9de5f46b5a86618e6d586d992e626b
doomslayer6464 <adithyan46a@gmail.com>
2026-03-03 13:27:33 +0530

    Add files via upload

---
+++ UC3.java
@@ -0,0 +1,58 @@
+/**
+ * OOPSBannerApp - UC3 Implementation
+ * Displays "OOPS" banner using String.join() for better memory efficiency.
+ *
+ * @author Adithyan
+ * @version 3.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                "****** "));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *"));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *"));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "****** "));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "));
+
+        System.out.println(String.join("   ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                "*      "));
+    }
+}
