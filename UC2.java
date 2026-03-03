a4b62371f336212b8f944783749ace7c7595dbb4
doomslayer6464 <adithyan46a@gmail.com>
2026-03-03 13:25:10 +0530

    Add files via upload

---
+++ UC2.java
@@ -0,0 +1,24 @@
+/**
+ * OOPSBannerApp - UC2 Implementation
+ * Displays "OOPS" as a large ASCII banner using '*' characters.
+ *
+ * @author Adithyan
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****     *****    ****** ");
+        System.out.println("*     *   *     *   *     *   *     *");
+        System.out.println("*     *   *     *   *     *   *     *");
+        System.out.println("*     *   *     *   *     *   ****** ");
+        System.out.println("*     *   *     *   *     *   *      ");
+        System.out.println("*     *   *     *   *     *   *      ");
+        System.out.println(" *****     *****     *****    *      ");
+    }
+}
